5822144440e728ae0554d182c98ffba09e9aea4e
Remmy Clay <remmy.lauren@gmail.com>
2017-10-25 06:44:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
     return `The line is currently empty.`;
   } else {
-     return `The line is currently: ` + currentLine;
+     return `The line is currently: ${currentLine}`;
   }
 }
